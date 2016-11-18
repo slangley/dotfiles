4862be81b7bacb55644443e1417b238c6a5c4848
Sean Langley <sean.langley@capco.com>
2016-11-18 16:08:05 -0500

    updates to my dotfiles

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CURDIR=`pwd`
+ln -sv "$CURDIR/.inputrc" ~
+ln -sv "$CURDIR/.vimrc" ~
+ln -sv "$CURDIR/.gitconfig" ~
+ln -sv "$CURDIR/.bash_profile" ~
+ln -sv "$CURDIR/.gitignore" ~
+
+#.osx
